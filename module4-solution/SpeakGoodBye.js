dbe7c939de0b57a33bf51da48090764208b9c5b8
sagartharali98 <sagartharali@gmail.com>
2020-05-26 17:41:14 +0530

    updated

--- module4-solution/SpeakGoodBye.js
+++ module4-solution/SpeakGoodBye.js
@@ -5,7 +5,7 @@
 // See Lecture 52, part 2
 (function(window) {
 	var byeSpeaker = new Object();
-	var speakWord = "Goodbye";
+	var speakWord = "Good Bye";
 	byeSpeaker.speak = function (name) {
 		  console.log(speakWord + " " + name);
 	};
